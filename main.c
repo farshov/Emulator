09bacae3d9dc0a1a3e9e30833ee171e07d705869
Igor Medvedev <igor.mipt@mail.ru>
2017-05-03 22:19:33 +0300

    Emulator v1.2
    Can add array of number

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
 #define BYTE_SIZE 8
 #define NO_PARAM 0
 #define HAS_SS 1
-#define HAS_DD 1<<1
+#define HAS_DD (1<<1)
+#define HAS_NN (1<<3)
+#define HAS_R (1<<2)
 #define PC 7
 
 typedef unsigned char byte;
@@ -14,6 +16,7 @@ typedef short word;
 typedef int adr;
 byte mem[64*1024];
 word reg[8] = {};  //регистор
+adr A = 0;
 
 byte b_read (adr a);
 void b_write (adr a, byte val);
@@ -25,8 +28,12 @@ void run_programme(adr start, word n);
 void do_halt();
 void do_mov();
 void do_add();
+void do_sob();
+void do_clr();
 void do_unknown();
-struct SSDD get_mr(word param, word cur);
+word get_r(word cur);
+word get_nn(word cur);
+struct MR get_mr(word cur);
 
 
 struct Command
@@ -38,22 +45,26 @@ struct Command
     word param;  // ss, dd, xx, n
 };
 
-typedef struct mr
+typedef struct MR
 {
     word val;
     adr a;
-} mr;
+} MR;
 
-typedef struct SSDD
+typedef struct PARAM
 {
-    mr ss;
-    mr dd;
-} SSDD;
+    word nn;
+    word r;
+    MR ss;
+    MR dd;
+} PARAM;
 
 const struct Command command_list[] = {
     {0xFFFF,  0,       "HALT", do_halt, NO_PARAM},
     {0170000, 0010000, "MOV", do_mov, HAS_SS | HAS_DD},
     {0170000, 0060000, "ADD", do_add, HAS_SS | HAS_DD},
+    {0177000, 0077000, "SOB", do_sob, HAS_R | HAS_NN},
+    {0xFF00, 0005000, "CLR", do_clr, HAS_DD},
     {0, 0, "unknown", do_unknown, NO_PARAM}
 };
 
@@ -74,15 +85,14 @@ int main(int argc, char * argv[])
 void run_programme(adr start, word n)
 {
 	word i = 0;
-    adr a = start;
     word cur = 0;
     reg[PC] = start; 
     while(1)
     {
-		cur = w_read(a);																
-        printf("%06x : %06x ", a, cur);
-        a += 2;
-        struct SSDD res = {};
+		cur = w_read(reg[PC]);																
+        printf("%06o : %06o ", reg[PC], cur);
+        reg[PC] += 2; 
+        struct PARAM res = {};
         struct Command cmd;
 
         for(i = 0; ; i++)
@@ -91,101 +101,95 @@ void run_programme(adr start, word n)
             if((cur & cmd.mask) == cmd.opcode)
             {
                 printf("%s ", cmd.name);
-                if(cmd.param)
-                {
-                    res = get_mr(cmd.param, cur);
-                }
+                if(cmd.param & HAS_SS)
+                    res.ss = get_mr(cur >> 6);
+                if(cmd.param & HAS_DD)
+                    res.dd = get_mr(cur);
+                if(cmd.param & HAS_R)
+                    res.r = get_r(cur);
+                if(cmd.param & HAS_NN)
+                    res.nn = get_nn(cur);
                 cmd.do_action(res);
                 break;
             }   
         }
-        printf("\n");     
+        printf("\n");    
     }
 }
 
-struct SSDD get_mr(word param, word cur)
+word get_r(word cur)
+{
+    return (cur>>6) & 7;
+}
+
+word get_nn(word cur)
+{
+    return cur & 0x3F;
+}
+
+struct MR get_mr(word cur)
 {
     adr adress = 0;
     int n = 0;
     int mode = 0;
-    struct SSDD res;
-    if(param & HAS_SS)
+    MR res;
+    n = cur & 7;
+    mode = (cur >> 3) & 7;
+    switch(mode)
     {
-        n = (cur >> 6) & 7;
-        mode = (cur >> 9) & 7;
-        switch(mode)
-        {
-            case 0  :     //R1                  //подумать над проблемой реализации
-                res.ss.a = n;
-                res.ss.val = reg[n];
-                printf("R%d ", n);
-                break;
-            case 1:  //(R1)
-                res.ss.a = reg[n];
-                res.ss.val = w_read(res.ss.a);
-                printf("(R%d) \n", n);
-                break;
-            case 2:
-            {
-                if(n == PC)
-                {
-                    reg[PC] += 2;
-                    adress = reg[PC];
-                    res.ss.a = n;
-                    res.ss.val = w_read(adress);
-                    reg[PC] += 2;
-                    printf("#%d ", res.ss.val);
-                }
-                else
-                {
-                    res.ss.a = reg[n];
-                    res.ss.val = w_read(res.ss.a);
-                    printf("(R%d)+ \n", n);
-                }
-                break;
-            }
+        case 0  :     //R1                  //подумать над проблемой реализации
+            res.a = n;
+            res.val = reg[n];
+            printf("R%d ", n);
+            break;
+        case 1:  //(R1) 
+            res.a = reg[n];
+            res.val = w_read(res.a);
+            printf("(R%d) \n", n);
+            break;
+        case 2:
             
-        }
+            res.a = reg[n];
+            reg[n] += 2;
+            res.val = w_read(res.a);
+            if(n == PC)
+            {
+                printf("#%d ", res.val);
+            }
+            else
+            {
+                printf("(R%d)+ ", n);
+            }
+            break;
+        case 3:
+            //reg[n] += 2;
+            res.a = w_read(reg[n]);
+            res.val = w_read(res.a);
+            reg[n] += 2;
+            if(n == PC)
+            {
+                printf("#%d ", res.a);
+            }
+            else
+            {
+                printf("@(R%d)+ ", n);
+            }
+            break;
+        case 4:
+            reg[n] -= 2;
+            res.a = reg[n];
+            res.val = w_read(res.a);
+            printf("-(R%d) ", n);
+            reg[n] -= 2;
+            break;
+        case 5:
+            reg[n] -= 2;
+            res.a = w_read(reg[n]);
+            res.val = w_read(res.a);
+            printf("@-(R%d) ", n);
+            reg[n] -= 2;
+            break;
     }
-    if(param & HAS_DD)
-    {
-        n = cur & 7;
-        mode = (cur >> 3) & 7;
-        switch(mode)
-        {
-            case 0  :     //R1                  //подумать над проблемой реализации
-                res.dd.a = n;
-                res.dd.val = reg[n];
-                printf("R%d ", n);
-                break;
-            case 1:  //(R1)
-                res.dd.a = reg[n];
-                res.dd.val = w_read(res.dd.a);
-                printf("(R%d) \n", n);
-                break;
-            case 2:
-                res.dd.a = reg[n];
-                res.dd.val = w_read(res.dd.a);
-                printf("(R%d)+ \n", n);
-                break;
-            case 3:
-                res.dd.a = reg[n];
-                res.dd.val = w_read(res.dd.a);
-                printf("@(R%d)+ \n", n);
-                break;
-            case 4:
-                res.dd.a = reg[n];
-                res.dd.val = w_read(res.dd.a);
-                printf("-(R%d) \n", n);
-                break;
-            case 5:
-                res.dd.a = reg[n];
-                res.dd.val = w_read(res.dd.a);
-                printf("@-(R%d) \n", n);
-                break;
-        }
-    }
-    
     return res;
 }
 
@@ -244,15 +248,21 @@ void w_write(adr a, word val)
 
 void do_halt()
 {
+    /*int i = 0;
+    printf("\n");
+    for(i = 0; i < 8; i ++)
+    {
+        printf("reg[%d] = %o\n", i, reg[i]);
+    }*/
     exit(0);
 }
 
-void do_mov(struct SSDD res)
+void do_mov(struct PARAM res)
 {
     reg[res.dd.a] = res.ss.val;
 }
 
-void do_add(struct SSDD res)
+void do_add(struct PARAM res)
 {
     reg[res.dd.a] = res.dd.val + res.ss.val;
     printf("\nAdd result: %d", reg[res.dd.a]);
@@ -261,4 +271,17 @@ void do_add(struct SSDD res)
 void do_unknown()
 {
     ;   
+}
+
+void do_sob(struct PARAM res)
+{
+    if(--reg[res.r] != 0)
+    {
+        reg[PC] = reg[PC] - 2 * res.nn;
+    }
+}
+
+void do_clr(struct PARAM res)
+{
+    reg[res.dd.a] = 0;
 }
