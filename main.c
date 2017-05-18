03ae46ec23e95b2eb17fc67d1a09f39b7f1a23ea
Igor Medvedev <igor.mipt@mail.ru>
2017-05-18 13:38:56 +0300

    jsr, rts, 6 mode, prit of "Hello, world!".

--- main.c
+++ main.c
@@ -17,6 +17,12 @@
 #define Z 2
 #define V 3
 #define C 4
+#define OSTAT 0xFF74
+#define ODATA 0xFF76
+#define ODATA_1 0xFFFFFF76
+#define MEM 1
+#define REG 0
+#define BYTE 1
 
 typedef unsigned char byte;
 typedef short word;
@@ -39,10 +45,14 @@ void do_sob();
 void do_clr();
 void do_beq();
 void do_br();
+void do_tst();
+void do_bpl();
+void do_rts();
+void do_jsr();
 void do_unknown();
 void reg_dump();
-void check_Z(word res);
-void check_N(word res);
+void change_Z(word res);
+void change_N(word res, word b);
 word get_xx(word cur);
 word get_r(word cur);
 word get_nn(word cur);
@@ -61,6 +71,8 @@ struct Command
 
 typedef struct MR
 {
+    word b;
+    word space;
     word val;
     adr a;
 } MR;
@@ -76,13 +88,18 @@ typedef struct PARAM
 
 const struct Command command_list[] = {
     {0xFFFF,  0,       "HALT", do_halt, NO_PARAM},
-    {0170000, 0010000, "MOV", do_mov, HAS_SS | HAS_DD | HAS_B},
-    {0170000, 0060000, "ADD", do_add, HAS_SS | HAS_DD},
-    {0177000, 0077000, "SOB", do_sob, HAS_R | HAS_NN},
+    {0xF000, 0010000, "MOV", do_mov, HAS_SS | HAS_DD | HAS_B},
+    {0xF000, 0060000, "ADD", do_add, HAS_SS | HAS_DD},
+    {0xFE00, 0077000, "SOB", do_sob, HAS_R | HAS_NN},
     {0xFF00,  0005000, "CLR", do_clr, HAS_DD},
-    {0170000, 0110000, "MOVb", do_mov, HAS_SS | HAS_DD | HAS_B},
-    {0xFFC0, 001400, "BEQ", do_beq, HAS_XX},
-    {0x0100, 000400, "BR", do_br, HAS_XX},
+    {0xF000, 0110000, "MOVb", do_mov, HAS_SS | HAS_DD | HAS_B},
+    {0xFF00,  0001400, "BEQ", do_beq, HAS_XX},
+    {0xFF00,  0000400, "BR", do_br, HAS_XX},
+    {0xFFC0,  0105700, "TSTb", do_tst, HAS_B | HAS_DD},
+    {0xFFC0,  0005700, "TST", do_tst, HAS_B | HAS_DD},
+    {0xFF00,  0100000, "BPL", do_bpl, HAS_XX},
+    {0xFE00,  0004000, "JSR", do_jsr, HAS_DD | HAS_R},
+    {0xFFF8,  0000200, "RTS", do_rts, HAS_R},
     {0, 0, "unknown", do_unknown, NO_PARAM}
 };
 
@@ -104,7 +121,9 @@ void run_programme(adr start, word n)
 {
 	word i = 0;
     word cur = 0;
-    reg[PC] = start; 
+    reg[PC] = start;
+    mem[OSTAT] = mem[OSTAT] | 0x80;
+    reg[SP] = 0x200;
     while(1)
     {
 		cur = w_read(reg[PC]);
@@ -128,15 +147,17 @@ void run_programme(adr start, word n)
                     res.ss = get_mr(cur >> 6);
                 if(cmd.param & HAS_DD)
                     res.dd = get_mr(cur);
-                if(cmd.param & HAS_R)
+                if((cmd.param & 0x1F) == HAS_R)
                     res.r = get_r(cur);
+                else if(cmd.param & HAS_R)
+                    res.r = get_r(cur >> 6);
                 if(cmd.param & HAS_NN)
                     res.nn = get_nn(cur);
                 if(cmd.param & HAS_XX)
                     res.xx = get_xx(cur);
                 cmd.do_action(res);
                 break;
-            }   
+            }    
         }
         printf("\n");    
     }
@@ -144,7 +165,7 @@ void run_programme(adr start, word n)
 
 word get_r(word cur)
 {
-    return (cur>>6) & 7;
+    return cur & 7;
 }
 
 word get_nn(word cur)
@@ -173,27 +194,30 @@ struct MR get_mr(word cur)
     int n = 0;
     int mode = 0;
     MR res;
-    word b = get_b(cur >> 15);
+    res.b = get_b(cur >> 15);
     n = cur & 7;
     mode = (cur >> 3) & 7;
+    word nn = 0;
     switch(mode)
     {
         case 0  :     //R1                  //подумать над проблемой реализации
             res.a = n;
             res.val = reg[n];
             printf("R%d ", n);
+            res.space = REG;
             break;
         case 1:  //(R1) 
             res.a = reg[n];
-            if(b == 1)
+            if(res.b == BYTE)
                 res.val = b_read(res.a);                
             else
                 res.val = w_read(res.a);
             printf("(R%d) \n", n);
+            res.space = MEM;
             break;
         case 2:
             res.a = reg[n];
-            if(b == 1)
+            if(res.b == BYTE)
             {
                 res.val = b_read(res.a);    // отрицательные числа                       
                 if(res.val >> 7 == 1)
@@ -211,58 +235,64 @@ struct MR get_mr(word cur)
                 reg[n] += 2;
             }
             if(n == PC)
-                printf("#%d ", res.val);
+            {
+                printf("#%o ", res.val);
+            }
             else
                 printf("(R%d)+ ", n);
+            res.a = n;
+            res.space = MEM;
             break;
         case 3:
-            res.a = w_read(reg[n]);
-            if(b == 1)
+            res.a = reg[n];
+            reg[n] += 2;
+            res.a = w_read(res.a)+ 0200000;
+            //printf("~res.a = %o~\n", res.a);
+            if(res.b == BYTE)
             {
                 res.val = b_read(res.a);  
                 if(res.val >> 7 == 1)
                     res.val = res.val | 0xFF00;
                 else
                     res.val = res.val | 0;                     
-                if(n == PC || n == SP)
-                    reg[n] += 2;
-                else
-                    reg[n] += 1;
             }
             else
             {
                 res.val = w_read(res.a);
-                reg[n] += 2;
             }       
             if(n == PC)
                 printf("@#%d ", res.val);
             else
                 printf("@(R%d)+ ", n);
+            res.space = MEM;
             break;
         case 4:
-            res.a = reg[n];
-            if(b == 1)
+            if(res.b == BYTE)
             {
+                reg[n] -= 1;
+                if(n == PC || n == SP)
+                {
+                    reg[n] -= 1;
+                }
+                res.a = reg[n];
                 res.val = b_read(res.a); 
                 if(res.val >> 7 == 1)
                     res.val = res.val | 0xFF00;
                 else
-                    res.val = res.val | 0;
-                if(n == PC || n == SP)
-                    reg[n] -= 2;
-                else
-                    reg[n] -= 1;                      
+                    res.val = res.val | 0;                    
             }
             else
             {
-                res.val = w_read(res.a);
                 reg[n] -= 2;
+                res.a = reg[n];
+                res.val = w_read(res.a);
             }
             printf("-(R%d) ", n);
+            res.space = MEM;
             break;
         case 5:
             res.a = w_read(reg[n]);
-            if(b == 1)
+            if(res.b == BYTE)
             {
                 res.val = b_read(res.a);
                 if(res.val >> 7 == 1)
@@ -281,7 +311,20 @@ struct MR get_mr(word cur)
             }
             printf("@-(R%d) ", n);
             reg[n] -= 2;
+            res.space = MEM;
             break;
+        case 6:
+            nn = w_read(reg[PC]);
+            reg[PC] += 2;
+            res.a = reg[n] + nn;
+            res.val = w_read(res.a);
+            printf(" %d(R%d) res.a = %o val : %o ", nn, n, res.a, res.val);
+            /*nn = w_read(reg[PC]);
+            reg[PC] += 2;
+            res.a = reg[n];
+            res.a += nn;
+            res.val = w_read(res.a);
+            printf("PC : %o  loop : %o", reg[PC], res.a);*/
     }
     return res;
 }
@@ -345,9 +388,42 @@ void do_halt()
     exit(0);
 }
 
+void do_rts(struct PARAM res)
+{
+    reg[PC] = reg[res.r];
+    reg[res.r] = w_read(reg[SP]); //!!!
+    reg[SP] += 2;
+}
+
+void do_jsr(struct PARAM res)
+{
+    reg[res.r] = reg[PC];
+    w_write(reg[SP] - 2, reg[res.r]);
+    reg[SP] -= 2;
+    reg[PC] = res.dd.a;
+}
+
 void do_br(struct PARAM res)
 {
     reg[PC] = reg[PC] + 2 * res.xx;
+}
+
+void do_tst(struct PARAM res)
+{
+    if(((res.dd.val >> 7) & 1) == 1)
+    {
+        PSW[N] = 1;
+    } 
+    else
+    {
+        PSW[N] = 0;
+    }
+}
+
+void do_bpl(struct PARAM res)
+{
+    if(PSW[N] == 0)
+        do_br(res);
 }
 
 void do_beq(struct PARAM res)
@@ -366,7 +442,7 @@ void reg_dump()
     }
 }
 
-void check_Z(word res)
+void change_Z(word res)
 {
     if(res == 0)
         PSW[Z] = 1;
@@ -374,9 +450,13 @@ void check_Z(word res)
         PSW[Z] = 0;
 }
 
-void check_N(word res)
+void change_N(word res, word b)
 {
-    if(res < 0)
+    if(b == BYTE)
+        res = res >> 7;
+    else
+        res = res >> 15;
+    if(res = 1)
         PSW[N] = 1;
     else
         PSW[N] = 0;
@@ -384,16 +464,33 @@ void check_N(word res)
 
 void do_mov(struct PARAM res)
 {
-    reg[res.dd.a] = res.ss.val;
-    check_Z(reg[res.dd.a]);
-    check_N(reg[res.dd.a]);
+    if(res.dd.a == ODATA || res.dd.a == ODATA_1)
+    {
+        printf("~~%c~~", res.ss.val);
+    }
+    if(res.dd.space == REG)
+    {
+        reg[res.dd.a] = res.ss.val;
+        change_Z(res.ss.val);        
+        change_N(res.ss.val, res.ss.b);
+    }
+    else if(res.dd.space == MEM)
+    {
+        if(res.ss.b == BYTE)
+            b_write(res.dd.a, res.ss.val);
+        else
+            w_write(res.dd.a, res.ss.val);
+        change_Z(res.ss.val);        
+        change_N(res.ss.val, res.ss.b);
+    }
+   // printf(" reg[1] = %o ", reg[1]);
 }
 
 void do_add(struct PARAM res)
 {
     reg[res.dd.a] = res.dd.val + res.ss.val;
-    check_Z(reg[res.dd.a]);
-    check_N(reg[res.dd.a]);
+    change_Z(reg[res.dd.a]);
+    change_N(reg[res.dd.a], res.ss.b);
     printf("\nAdd result: %o", reg[res.dd.a]);
 }
 
